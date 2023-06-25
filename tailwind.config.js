87d10746c2768f1b01fec4af74f0b0f1f742dfce
Joshua Lloyd <100847867+jlloyd5848@users.noreply.github.com>
2023-06-25 21:48:14 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-
-
-// module.exports = {
-//   purge: {
-//     content: [
-//       './resources/**/*.blade.php',
-//       './resources/**/*.js',
-//       './resources/**/*.vue',
-//     ]
-//   },
-//   // rest of your Tailwind CSS configuration...
-// }
 module.exports = {
   purge: [],
   darkMode: false,
